3b5052923aeb7f9e279c7db8de081ac38540f7cd
Leo <leo.jasper@studerande.plushogskolan.se>
2025-05-07 12:22:31 +0200

    Create listing using @RequestHeader from backend. Added simple validation and error handling

--- src/api/service.js
+++ src/api/service.js
@@ -1,13 +1,30 @@
 import api from "./axios";
 
-// hämta produkter
-export const getAllProducts = async () => {
-  const response = await api.get("/products");
-  return response.data;
-};
 
-// hämta en produkt med id
-export const getProductById = async (id) => {
-  const response = await api.get(`/products/${id}`);
+// get all listings
+export const getAllListings = async () => {
+  const response = await api.get("/listing");
   return response.data;
-};
+}
+
+// get a specific listing
+export const getListingById = async (id) => {
+  const response = await api.get(`/listing/${id}`);
+  return response.data;
+}
+
+// Create listing. Backend using @RequestHeader to authenticate a user, so i need to send the jwt token in the header
+// listingData is just everything relevant inside the listing model (title, rooms, description etc) in backend
+
+//IMPORTANT: RIGHT NOW IT LOOKS FOR THE TOKEN INSIDE LOCAL STORAGE
+//IT SHOULD BE IN THE COOKIES
+
+export const createListing = async (listingData) => {
+  const response = await api.post("/listing/create", listingData, {
+    headers: {
+      "Authorization": `Bearer ${localStorage.getItem("jwt")}`, //not sure how to get the token from cookies right now
+      "Content-Type": "application/json",
+    },
+  });
+    return response.data;
+}
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,85 +1,93 @@
-import Form from 'react-bootstrap/Form';
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
-import { Button } from 'react-bootstrap';
+import { createListing } from "../api/service.js";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const NewListing = () => {
+    const [title, setTitle] = useState("");
+    const [rooms, setRooms] = useState(0);
+    const [pricePerNight, setPricePerNight] = useState(0);
+    const [description, setDescription] = useState("");
+    const [location, setLocation] = useState("");
+    const [imageUrl, setImage] = useState("");
+    const [startDate, setStartDate] = useState(new Date().toISOString().split("T")[0]);
+    const [endDate, setEndDate] = useState(new Date().toISOString().split("T")[0]);
+    const navigate = useNavigate();
 
-const [hostId, sethostId] = useState("");
-const [title, setTitle] = useState("");
-const [rooms, setRooms] = useState(0);
-const [pricePerNight, setPricePerNight] = useState(0);
-const [description, setDescription] = useState("");
-const [location, setLocation] = useState("");
-const [imageUrl, setImage] = useState("");
-const [startDate, setStartDate] = useState(new Date().toISOString().split("T")[0]);
-const [endDate, setEndDate] = useState(new Date().toISOString().split("T")[0]);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            //simple validation for now
+            if (!title || !rooms || !pricePerNight || !description || !location || !imageUrl || !startDate || !endDate) {
+                throw new Error("Please fill in all required fields");
+            }
 
-const navigate = useNavigate();
-  // konsumerar contexten
-  const { createListing } = useAuth();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      await createListing(title, rooms, pricePerNight, description, location, image, startDate, endDate);
-      navigate("/");
-    } catch (err) {
-      console.log("error: " + err);
+                const listingData = {
+                title,
+                rooms: Number(rooms),
+                pricePerNight: Number(pricePerNight),
+                description,
+                location,
+                imageUrl,
+                availability: [
+                    {
+                        //Need to convert to ISO string to deserialize in backend
+                        startDate: new Date(startDate).toISOString(),
+                        endDate: new Date(endDate).toISOString()
+                    }
+                ]
+            };
+            // try catch to debug when submitting
+            const success = await createListing(listingData);
+            console.log("Listing created successfully:", success);
+            navigate("/listing");
+        }
+        catch (err) {
+            console.error("Error creating listing:", err);
+        }
     }
-  };
 
-  return (
-      <Form className='listing-container' onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicTitle">
-      <Form.Label>Title</Form.Label>
-      <Form.Control type="text" placeholder="Enter Listing Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
-      </Form.Group>
+    return (
+        <Form className='listing-container' onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicTitle">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" placeholder="Enter Listing Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicRooms">
+                <Form.Label>Rooms</Form.Label>
+                <Form.Control type="number" placeholder="Rooms" value={rooms} onChange={(e) => setRooms(Number(e.target.value))} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPrice">
+                <Form.Label>Price / Night</Form.Label>
+                <Form.Range min="0" max="1000" value={pricePerNight} onChange={(e) => setPricePerNight(Number(e.target.value))} />
+            </Form.Group>
+            <Form.Group className="description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" placeholder="Enter Listing Description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="location">
+                <Form.Label>Location</Form.Label>
+                <Form.Control type="text" placeholder="Enter Listing Location" value={location} onChange={(e) => setLocation(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="location">
+                <Form.Label>Image</Form.Label>
+                <Form.Control type="url" placeholder="Enter Listing Image" value={imageUrl} onChange={(e) => setImage(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicStartDate">
+                <Form.Label>Start Date</Form.Label>
+                <Form.Control type="date" value={startDate} min={new Date().toISOString().split("T")[0]} onChange={(e) => setStartDate(e.target.value)} required/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEndDate">
+                <Form.Label>End Date</Form.Label>
+                <Form.Control type="date" value={endDate} min={startDate} onChange={(e) => setEndDate(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Button variant="secondary" type="submit " className="mt-3" onSubmit={handleSubmit}></Button>
+            </Form.Group>
+        </Form>
 
-      <Form.Group className="mb-3" controlId="formBasicHostId">
-      <Form.Label>Host</Form.Label>
-      <Form.Control type="text" value={hostId} onChange={(e) => sethostId(e.target.value)}/>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicRooms">
-      <Form.Label>Rooms</Form.Label>
-      <Form.Control type="number" placeholder="Rooms" value={rooms} onChange={(e) => setRooms(Number(e.target.value))} />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPrice">
-      <Form.Label>Price / Night</Form.Label>
-      <Form.Range min="0" max="1000" value={pricePerNight} onChange={(e) => setPricePerNight(Number(e.target.value))} />
-      </Form.Group>
-      
-
-      <Form.Group className="description">
-      <Form.Label>Description</Form.Label>
-      <Form.Control type="text" placeholder="Enter Listing Description" value={description} onChange={(e) => setDescription(e.target.value)}/>
-      </Form.Group>
-
-      <Form.Group className="location">
-      <Form.Label>Location</Form.Label>
-      <Form.Control type="text" placeholder="Enter Listing Location" value={location} onChange={(e) => setLocation(e.target.value)}/>
-      </Form.Group>
-
-      <Form.Group className="location">
-      <Form.Label>Image</Form.Label>
-      <Form.Control type="url" placeholder="Enter Listing Image" value={imageUrl} onChange={(e) => setImage(e.target.value)}/>
-      </Form.Group>
-
-      <Form.Group className="location">
-      <Form.Label>Pick start date</Form.Label>
-      <Form.Control type="date" placeholder="Enter Listing Start Date" value={startDate} onChange={(e) => setStartDate(e.target.value)}/>
-      <Form.Label>Pick end date</Form.Label>
-      <Form.Control type="date" placeholder="Enter Listing End Date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
-      <Button variant="secondary" type="submit" className='listing-submit' >Create Listing</Button>
-      </Form.Group>
-      </Form>
-
-
-  );
+    );
 }
 
 export default NewListing;
