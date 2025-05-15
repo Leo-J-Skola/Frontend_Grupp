7778eeb20d1137e65c32749e3cd1367fcccb745e
Axel Liljendal <liljendalaxel@gmail.com>
2025-05-15 20:56:32 +0200

    removed camelcase from getUserByUsername

--- src/api/profileService.js
+++ src/api/profileService.js
@@ -2,8 +2,7 @@ import api from "./axios";
 import Cookies from "js-cookie";
 
 // get a specific user by username 
-export const getUserByUserName = async (username) => {
-  const response = await api.get(`/user/findByUserName/${username}`);
+export const getUserByUsername = async (username) => {
+  const response = await api.get(`/user/findByUsername/${username}`);
   return response.data;
 }
-
